f1a8d7e3b4ea4d8c8a913e5537e8408edffbff0b
Surendra Kolhe <surendrakolhe.kolhe@gmail.com>
2023-07-19 22:23:18 +0530

    Update the calls to pass the multiplier

--- obj/kdTreeInterp.f90
+++ obj/kdTreeInterp.f90
@@ -1,56 +1,66 @@
-module call_kd_tree
-    use iso_c_binding
-    implicit none
-    interface
+MODULE call_kd_tree
+    USE iso_c_binding
+    IMPLICIT NONE
 
-        subroutine custom_funct(coordinates, numPoints, numQuerys, query, results, numResults, rootIdx) bind(C, name="custom_funct")
-            use iso_c_binding
-            real(c_float), dimension(*) :: coordinates
-            integer(c_int), value :: numPoints
-            integer(c_int), value :: numQuerys
-            real(c_float), dimension(*) :: query
-            real(c_float), dimension(*) :: results
-			integer(c_int), value :: numResults
-			integer(c_int), intent(inout) :: rootIdx
-        end subroutine
+    !*************************************C INTERFACE***************************************
+    INTERFACE
+        SUBROUTINE custom_funct(coordinates, numPoints, numQuerys, query, results, numResults, rootIdx) bind(C, name="custom_funct")
+            USE iso_c_binding
+            REAL(c_float), dimension(*) :: coordinates
+            INTEGER(c_int), value :: numPoints
+            INTEGER(c_int), value :: numQuerys
+            REAL(c_float), dimension(*) :: query
+            REAL(c_float), dimension(*) :: results
+			INTEGER(c_int), value :: numResults
+			INTEGER(c_int), intent(inout) :: rootIdx
+        END SUBROUTINE
 
-		subroutine search_funct(coordinates, numDimensions, numQuerys, query, results, numResults, rootIdx) bind(C, name="search_funct")
-            use iso_c_binding
-            real(c_float), dimension(*) :: coordinates
-            integer(c_int), value :: numDimensions
-            integer(c_int), value :: numQuerys
-            real(c_float), dimension(*) :: query
-			real(c_float), dimension(*) :: results
-			integer(c_int), value :: numResults
-			integer(c_int), value:: rootIdx
-        end subroutine
+		SUBROUTINE search_funct(coordinates, numDimensions, numQuerys, query, results, numResults, rootIdx, mltip) &
+         bind(C, name="search_funct")
+            USE iso_c_binding
+            REAL(c_float), dimension(*) :: coordinates
+            INTEGER(c_int), value :: numDimensions
+            INTEGER(c_int), value :: numQuerys
+            REAL(c_float), dimension(*) :: query
+			REAL(c_float), dimension(*) :: results
+			INTEGER(c_int), value :: numResults
+			INTEGER(c_int), value:: rootIdx
+            REAL(c_float), value :: mltip
+        END SUBROUTINE
+    END INTERFACE
+END MODULE call_kd_tree
 
-    end interface
-end module call_kd_tree
+MODULE arrs
+    INTEGER,ALLOCATABLE :: NI(:), NJ(:), NK(:), startblk(:), endblk(:)
+    INTEGER nblocks
+END MODULE arrs
 
 
-program main
+PROGRAM main
 
-use call_kd_tree
-implicit none
+USE call_kd_tree
+USE arrs
+IMPLICIT NONE
 
-integer(c_int) :: numPoints, numResults, rootIdx    
-integer :: numDimensions = 3
-integer :: sizeOfarray 
-real(c_float), allocatable :: coordinates(:), results(:)
-real(c_float), allocatable :: query(:)
-integer(c_int) :: numQuerys
-integer :: nblocks, i, j, k, index
-integer, allocatable :: NI(:), NJ(:), NK(:)
-real, allocatable :: xgrid(:,:,:,:), ygrid(:,:,:,:), zgrid(:,:,:,:)
-character(100) :: filename="grid.xyz"
-real :: Lx, Ly, Lz
-Real,ALLOCATABLE :: Igrid(:,:,:,:), Jgrid(:,:,:,:), Kgrid(:,:,:,:)
-Real,ALLOCATABLE :: Xgridnew(:,:,:,:), Ygridnew(:,:,:,:), Zgridnew(:,:,:,:)
-Real,ALLOCATABLE :: Igridnew(:,:,:,:), Jgridnew(:,:,:,:), Kgridnew(:,:,:,:)
-integer nbl, NImax, NJmax, NKmax
-integer nblocksnew,NImaxnew, NJmaxnew, NKmaxnew
-Integer,ALLOCATABLE :: NInew(:), NJnew(:), NKnew(:)
+!***********************************declare variables**********************************
+
+INTEGER(c_int) :: numPoints, numResults, rootIdx    
+INTEGER :: numDimensions = 3
+INTEGER :: sizeOfarray 
+REAL(c_float):: mltip=100000.0
+REAL(c_float), ALLOCATABLE :: coordinates(:), results(:)
+REAL(c_float), ALLOCATABLE :: query(:)
+INTEGER(c_int) :: numQuerys
+INTEGER :: i, j, k, index
+REAL, ALLOCATABLE :: xgrid(:,:,:,:), ygrid(:,:,:,:), zgrid(:,:,:,:)
+CHARACTER(100) :: filename="grid.xyz"
+REAL :: Lx, Ly, Lz
+REAL,ALLOCATABLE :: Igrid(:,:,:,:), Jgrid(:,:,:,:), Kgrid(:,:,:,:)
+REAL,ALLOCATABLE :: Xgridnew(:,:,:,:), Ygridnew(:,:,:,:), Zgridnew(:,:,:,:)
+REAL,ALLOCATABLE :: Igridnew(:,:,:,:), Jgridnew(:,:,:,:), Kgridnew(:,:,:,:)
+INTEGER nbl, NImax, NJmax, NKmax
+INTEGER nblocksnew,NImaxnew, NJmaxnew, NKmaxnew
+INTEGER,ALLOCATABLE :: NInew(:), NJnew(:), NKnew(:)
 
 
     !**********************************Background block*****************************************
@@ -163,13 +173,13 @@ numPoints = size(xgrid)
 sizeOfarray = numPoints*numDimensions
 numQuerys = size(Xgridnew)
 numResults = 2
-allocate(results(numResults*numDimensions*numQuerys))
+ALLOCATE(results(numResults*numDimensions*numQuerys))
 
-allocate(coordinates(sizeOfarray))
-do nbl = 1, nblocks
-    do k = 1, NK(nbl)
-        do j = 1, NJ(nbl)
-            do i = 1, NI(nbl)
+ALLOCATE(coordinates(sizeOfarray))
+DO nbl = 1, nblocks
+    DO k = 1, NK(nbl)
+        DO j = 1, NJ(nbl)
+            DO i = 1, NI(nbl)
             
             ! Get the current index
             !index = (nbl-1)*NI(nbl)*NJ(nbl)*NK(nbl)*numDimensions + (k-1)*NI(nbl)*NJ(nbl)*numDimensions + (j-1)*NI(nbl)*numDimensions +  (i-1)*numDimensions + 1
@@ -181,26 +191,26 @@ do nbl = 1, nblocks
 
             
             !store in appropriate indices
-            coordinates(index) = Xgrid(i,j,k,nbl)*100000
-            coordinates(index+1) = Ygrid(i,j,k,nbl)*100000
-            coordinates(index+2) = Zgrid(i,j,k,nbl)*100000
+            coordinates(index) = Xgrid(i,j,k,nbl)*mltip
+            coordinates(index+1) = Ygrid(i,j,k,nbl)*mltip
+            coordinates(index+2) = Zgrid(i,j,k,nbl)*mltip
             
-            end do
-        end do
-    end do
-end do
+            END DO
+        END DO
+    END DO
+END DO
 !***************************************call creation of KD tree******************************************
 
-call custom_funct(coordinates, numPoints, numQuerys, query, results, numResults, rootIdx)
+CALL custom_funct(coordinates, numPoints, numQuerys, query, results, numResults, rootIdx)
 
 !***************************************Prepartion to call searching of KD tree******************************************
 
-allocate(query(numQuerys*numDimensions))
+ALLOCATE(query(numQuerys*numDimensions))
 
-do nbl = 1, nblocksnew
-    do k = 1, NKnew(nbl)
-        do j = 1, NJnew(nbl)
-            do i = 1, NInew(nbl)
+DO nbl = 1, nblocksnew
+    DO k = 1, NKnew(nbl)
+        DO j = 1, NJnew(nbl)
+            DO i = 1, NInew(nbl)
             
             ! Get the current index
             index = (nbl-1)*NInew(nbl)*NJnew(nbl)*NKnew(nbl)*numDimensions + &
@@ -214,36 +224,36 @@ do nbl = 1, nblocksnew
             query(index+1) = Ygridnew(i,j,k,nbl)
             query(index+2) = Zgridnew(i,j,k,nbl)
             
-            end do
-        end do
-    end do
-end do
+            END DO
+        END DO
+    END DO
+END DO
 
 !***************************************call searching of KD tree******************************************
 
-call search_funct(coordinates, numDimensions, numQuerys, query, results, numResults, rootIdx) 	
+CALL search_funct(coordinates, numDimensions, numQuerys, query, results, numResults, rootIdx, mltip) 	
 
 !***************************************Print Result******************************************
-!i=1
-!j=1
 
-!write(*,*) "Closest points points found are as follows : "
-!do while (i<=size(results))
-!	write(*, *) "X (", j, ") : ", results(i)
-!	write(*, *) "Y (", j, ") : ", results(i+1)
-!	write(*, *) "Z (", j, ") : ", results(i+2)
-!	j=j+1
-!	i=i+3
-!end do
+i=1
+j=1
+WRITE(*,*) "Closest points points found are as follows : "
+DO WHILE (i<=size(results))
+	WRITE(*, *) "X (", j, ") : ", results(i)
+	WRITE(*, *) "Y (", j, ") : ", results(i+1)
+	WRITE(*, *) "Z (", j, ") : ", results(i+2)
+	j=j+1
+	i=i+3
+END DO
 
-end program main
+END PROGRAM main
 
 !*************************************Local Index subroutine***************************************
 SUBROUTINE get_loc_index(global_index,blk,iloc,jloc,kloc)
-use arrs
-    integer global_index, rest_index
-    integer blk, iloc, jloc, kloc, nbl
-    integer sblk,eblk
+USE arrs
+    INTEGER global_index, rest_index
+    INTEGER blk, iloc, jloc, kloc, nbl
+    INTEGER sblk,eblk
     sblk=0
     !$acc data present(startblkl, endblkl, NI, NJ, NK)
 
@@ -256,15 +266,15 @@ use arrs
         sblk=eblk
     END DO
 
-	rest_index = global_index - sblk
-	kloc = int(rest_index/(NJ(blk)*NI(blk)))
-	rest_index = rest_index - kloc*NI(blk)*NJ(blk)
-	jloc = int(rest_index/(NI(blk)))
-	rest_index = rest_index - jloc*NI(blk)
-	iloc = int(rest_index)
-	
-	iloc = iloc+1
-	jloc = jloc+1
-	kloc = kloc+1
-	
-END	
+    rest_index = global_index - sblk
+    kloc = int(rest_index/(NJ(blk)*NI(blk)))
+    rest_index = rest_index - kloc*NI(blk)*NJ(blk)
+    jloc = int(rest_index/(NI(blk)))
+    rest_index = rest_index - jloc*NI(blk)
+    iloc = int(rest_index)
+    
+    iloc = iloc+1
+    jloc = jloc+1
+    kloc = kloc+1
+        
+END SUBROUTINE
